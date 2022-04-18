e7b699c8697b9986daf97d9249bf6af4600dbf36
Luiz Bonini <lc.bonini13@gmail.com>
2022-04-18 17:52:34 -0300

    Criação da Home Page

--- src/FullPages/FullPages.jsx
+++ src/FullPages/FullPages.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../Pages/Header/Header';
+import Landing from '../Pages/Landing/Lading';
 
 import { FullPagesStyle } from './FullPagesStyle';
 
@@ -7,6 +8,7 @@ const FullPages = () => {
     return ( 
     <FullPagesStyle>
         <Header/>
+        <Landing/>
     </FullPagesStyle> );
 }
  
--- src/Pages/Landing/LandingStyle.jsx
+++ src/Pages/Landing/LandingStyle.jsx
@@ -2,4 +2,43 @@ import styled from 'styled-components';
 
 export const LandingStyle = styled.div`
 
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+    .home-blob {
+        width: 30rem;
+
+        .home__blob-img {
+            width: 8rem;
+        }
+    }
+
+    .hello {
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 30rem;
+
+        h3 {
+            font-size: 2rem;
+        }
+
+        h1 {
+            font-size: 3rem;
+        }
+
+        p {
+            font-size: 1.3rem;
+            font-weight: 600;
+        }
+
+        span {
+            color: var(--cll-primary);
+        }
+
+    }
+
+
 `;
